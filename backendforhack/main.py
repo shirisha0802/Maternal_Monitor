0530665a22d8e1f738ae60db56c5a4f9fcdb0e93
shirisha0802 <shirisha.0802@gmail.com>
2026-09-23 16:03:03 +0530

    finally cors fixed

--- backendforhack/main.py
+++ backendforhack/main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://maternal-monitor.vercel.app/"
+        "https://maternal-monitor.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
